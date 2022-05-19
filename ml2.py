62fcb93832fa5ac78269a3fd71b938acce425838
Tejas Shah <52822453+Hero4440@users.noreply.github.com>
2022-05-19 19:44:29 +0530

    decision tree with visual

--- ml2.py
+++ ml2.py
@@ -1,15 +1,12 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier as dtc
-
-labelencoder_x = LabelEncoder()
 dataset = pd.read_csv("ml2.csv")
 dataset = dataset.apply(LabelEncoder().fit_transform)
-
-x = dataset.iloc[ : ,  : -1]
+x = dataset.iloc[:,:-1]
 y = dataset['Buys']
-x = np.asarray(x)
-c = dtc().fit(x, y)
-
-print(c.predict([[2, 0, 1, 0],[1,0,0,0]]))
+c = dtc().fit(x,y)
+print(c.predict([[2,0,1,0],[1,0,0,0],[1,0,0,0]]))
+from sklearn import tree
+tree.plot_tree(c)
